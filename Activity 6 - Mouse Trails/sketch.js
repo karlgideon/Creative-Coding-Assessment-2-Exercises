e4af58468f5c1fa0ca76e7ab3530e9e223834d84
karlgideon <karlgidladao@gmail.com>
2026-05-07 15:35:13 +0400

    Add files via upload

---
+++ Activity 6 - Mouse Trails/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+  background(48, 25, 52);
+}
+
+function draw() {
+  
+  noFill()
+  stroke(255, 192, 153)
+  circle(mouseX, mouseY, 50)
+}
+
+function mousePressed()
+{
+  background(48, 25, 52)
+}
